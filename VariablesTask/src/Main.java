1634a3437459ef2398a9bf0f69aae539025e2f96
Janna Almuqaisib <jannakalmuqaisib@gmail.com>
2024-11-03 10:17:38 +0300

    Add files via upload

---
+++ VariablesTask/src/Main.java
@@ -0,0 +1,31 @@
+public class Main {
+    public static void main(String[] args) {
+        // FIRST PART - COMMENT LAST SECTION TO RUN
+//        String firstName = "John";
+//        String lastName = "Doe";
+//        int age = 24;
+//        int someNumber = 178;
+//        System.out.println(firstName);
+//        System.out.println(lastName);
+//        System.out.println(age);
+//        System.out.println(someNumber);
+//        boolean married = true;
+//        double amount = 30;
+//        String deposit = "Deposit";
+//        String withdraw = "Withdraw";
+//        System.out.printf("The amount is %.0fKD%n", amount);
+//        System.out.printf("Would you like to %s or %s the amount?", deposit, withdraw);
+
+        // SECOND PART - COMMENT FIRST SECTION TO RUN
+
+//        int amount = 5;
+//        System.out.println(amount + " plus " + amount + " makes " + (amount + amount));
+//        String fullName = "John doe";
+//        String[] splitName = fullName.split(" ");
+//        int nameLength = splitName[1].length();
+//        String firstName = splitName[0];
+//        System.out.printf("My name is %s and my last name length is %d%n", firstName.toUpperCase(), nameLength);
+//        boolean isNameStartsWithD = splitName[1].startsWith("d");
+//        System.out.println(isNameStartsWithD);
+    }
+}
